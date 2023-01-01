41f17f501191383d5e740645bc426c56c6eb215f
Corentin Bacqué-Cazenave <corentin@progaccess.net>
2023-01-01 22:01:29 +0100

    Experimental new name and slogan

--- locales/it/default.tr.php
+++ locales/it/default.tr.php
@@ -3,7 +3,7 @@
   '_todo_level' => 4,
   '_last_author' => 'Pascal',
   '_last_modif' => 1544621411,
-  'sitename' => 'ProgAccess',
-  'slogan' => 'Accessibile e libero',
+  'sitename' => 'CrossAccess',
+  'slogan' => 'Accessibilità ovunque, da parte di tutti, per tutti',
   'banner_homelink' => 'Pagina principale',
 ) ?>
