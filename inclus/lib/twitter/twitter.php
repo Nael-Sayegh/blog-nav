92dab15dee73cbefec03f68488ab034fbf531f14
Corentin Bacqué-Cazenave <corentin@progaccess.net>
2023-01-23 20:42:17 +0100

    Fix Twitter posting script

--- inclus/lib/twitter/twitter.php
+++ inclus/lib/twitter/twitter.php
@@ -1,11 +1,11 @@
 <?php
-require_once 'inclus/config.local.php';
-require_once('../ca-bundle/src/CaBundle.php');
-require_once('inclus/lib/twitteroauth/autoload.php');
+$document_root = __DIR__.'/../../..';
+require_once $document_root.'/inclus/config.local.php';
+require_once $document_root.'/inclus/lib/ca-bundle/src/CaBundle.php';
+require_once $document_root.'/inclus/lib/twitteroauth/autoload.php';
 use Abraham\TwitterOAuth\TwitterOAuth;
 function send_twitter($message) {
 	$connection = new \Abraham\TwitterOAuth\TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
 	$post_tweets = $connection->post("statuses/update", ["status" => $message]);
 }
-send_twitter("Test");
 ?>
