d75eb53f3e44efa64da5d0cdb37711187f9a58a7
Corentin Bacqué-Cazenave <corentin@progaccess.net>
2023-01-23 20:27:21 +0100

    Add Composer/Ca-Bundle to work with TwitterOAuth

--- inclus/lib/twitter/twitter.php
+++ inclus/lib/twitter/twitter.php
@@ -1,10 +1,11 @@
 <?php
-$document_root = __DIR__.'/../../..';
-require_once $document_root.'/inclus/config.local.php';
-require_once($document_root.'/inclus/lib/twitteroauth/autoload.php');
+require_once 'inclus/config.local.php';
+require_once('../ca-bundle/src/CaBundle.php');
+require_once('inclus/lib/twitteroauth/autoload.php');
 use Abraham\TwitterOAuth\TwitterOAuth;
 function send_twitter($message) {
-	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+	$connection = new \Abraham\TwitterOAuth\TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
 	$post_tweets = $connection->post("statuses/update", ["status" => $message]);
 }
+send_twitter("Test");
 ?>
