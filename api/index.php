85759db7f8e7014f0c344c75125ef2c421750d7a
Corentin BACQUÉ-CAZENAVE <corentin@progaccess.net>
2023-08-26 22:02:25 +0200

    Change to get by ca=id - Update doc - Change API version

--- api/index.php
+++ api/index.php
@@ -68,13 +68,12 @@ include 'include/menu.php'; ?>
 						<li><em>[8]</em>: date</li>
 					</ul>
 				</li>
-				<li aria-level="2"><strong>c</strong>(=id)?: Catégories
+				<li aria-level="2"><strong>c</strong>: Catégories
 					<ul>
 						<li><em>[0]</em>: id</li>
 						<li><em>[1]</em>: name</li>
 						<li><em>[2]</em>: text</li>
 					</ul>
-					<p>Si un ID est spécifié, les articles de la catégorie seront retournés avec leur contenu.</p>
 				</li>
 				<li aria-level="2"><strong>a</strong>(=id)?: Articles
 					<ul>
