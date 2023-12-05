d0a920b580fe4459ca5865d4cb45a955a50725a5
Corentin BACQUÉ-CAZENAVE <corentin@progaccess.net>
2023-12-05 23:42:43 +0100

    Don't display team ID in contact page

--- contact.php
+++ contact.php
@@ -17,7 +17,7 @@ require_once('include/load_sound.php'); ?>
 $teamlist = '';
 $req = $bdd->query('SELECT * FROM `team` WHERE `works` = 1 OR `works` = 2');
 while($data = $req->fetch()) {
-	$teamlist .= '<li style="margin-top:8px;margin-bottom:8px;"><span class="rk rk_a">'.$data['short_name'].'</span> <span style="color:#505050;">(E'.$data['id'].')</span>, '.$data['status'].'<br><span style="color:#047d04;padding-left:16px;">'.tr($tr, 'teamlist_item', array('age'=>intval((time()-$data['age'])/31557600), 'date'=>date('d/m/Y',$data['date']))).'</span><p style="margin-top:0;padding-left:16px;color:#202000;">'.$data['bio'].'</p></li>';
+	$teamlist .= '<li style="margin-top:8px;margin-bottom:8px;"><span class="rk rk_a">'.$data['name'].'</span>, '.$data['status'].'<br><span style="color:#047d04;padding-left:16px;">'.tr($tr, 'teamlist_item', array('age'=>intval((time()-$data['age'])/31557600), 'date'=>date('d/m/Y',$data['date']))).'</span><p style="margin-top:0;padding-left:16px;color:#202000;">'.$data['bio'].'</p></li>';
 	
 }
 echo tr($tr,'maintext',array('teamlist'=>$teamlist,'lastv'=>$versionnom,'lastvdate'=>$versiondate,'lastvid'=>$derniereversion,'lastvopensource'=>$lastosv,'lastvu'=>$versionid));
