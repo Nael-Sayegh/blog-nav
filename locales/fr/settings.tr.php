5f21a037c18747a68fdd44a3fc87e7ed30127385
Nael-Sayegh <naelousay@gmail.com>
2025-06-22 14:28:38 +0200

    Update French translation

--- locales/fr/settings.tr.php
+++ locales/fr/settings.tr.php
@@ -3,8 +3,8 @@
 $tr = [
   '_' => 'settings',
   '_todo_level' => 0,
-  '_last_author' => 'Corentin',
-  '_last_modif' => 1618906113,
+  '_last_author' => 'Nael Sayegh',
+  '_last_modif' => 1750595318,
   'title' => 'Paramètres',
   'maintext' => '<p>Cette page vous donne accès à quelques paramètres pour personnaliser le site à votre guise.<br>
 Si vous n\'êtes pas identifié en tant que membre, les paramètres seront sauvegardés grâce à des cookies, sinon, ils seront enregistrés dans notre base de données et vous les retrouverez dès lors que vous vous identifierez sur le site (peu importe l\'appareil utilisé).</p>',
@@ -15,8 +15,6 @@ Si vous n\'êtes pas identifié en tant que membre, les paramètres seront sauve
   16 => '16 (Normal/par défaut)',
   20 => '20 (Grand)',
   24 => '24 (Très grand)',
-  'audio' => 'Audio',
-  'soundsvolume' => 'Volume des sons du site (en %) (0 pour désactivé)&nbsp;:',
   'slider' => 'Activer les infos défilantes de l\'accueil (défaut=oui)',
   'savebtn' => 'Enregistrer',
   'resetbtn' => 'Réinitialiser les paramètres par défaut',
