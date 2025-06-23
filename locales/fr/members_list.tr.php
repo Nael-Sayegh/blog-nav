5aef73c15fda83a7c9d4648fa4e55e73745b03a3
Nael-Sayegh <naelousay@gmail.com>
2025-06-23 20:29:48 +0200

    Fix syntaxe in array

--- locales/fr/members_list.tr.php
+++ locales/fr/members_list.tr.php
@@ -4,7 +4,7 @@ $tr = [
   '_' => 'members_list',
   '_todo_level' => 0,
   '_last_author' => 'Nael Sayegh',
-  '_last_modif' => 1750703236,
+  '_last_modif' => 1750703389,
   'title' => 'Liste des membres {{site}}',
   'sort_by' => 'Trier par&nbsp;:',
   'sort_by_signup_date' => 'Date d\'inscription',
@@ -14,5 +14,5 @@ $tr = [
   'table_name' => 'Nom',
   'table_signup' => 'Inscription',
   'table_rank' => 'Rang',
-  'table_count_members' => '<p><b>{{count}}</b> membres trouvés</p>';
+  'table_count_members' => '<p><b>{{count}}</b> membres trouvés</p>',
 ];
