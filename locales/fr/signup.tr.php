c9c021568c76ee42d097624d60f4ae264fc40889
Nael-Sayegh <naelousay@gmail.com>
2025-06-23 21:35:33 +0200

    Add translation for signup

---
+++ locales/fr/signup.tr.php
@@ -0,0 +1,29 @@
+<?php
+
+$tr = [
+  '_' => 'signup',
+  '_todo_level' => 0,
+  '_last_author' => 'Nael Sayegh',
+  '_last_modif' => 1750707333,
+  'title' => 'Se créer un compte {{site}}',
+  'log_lenght_username' => 'Votre nom d\'utilisateur doit comporter entre 3 et 32 caractères.',
+  'log_lenght_mail' => 'Votre adresse e-mail ne doit pas dépasser 255 caractères.',
+  'log_diff_psw' => 'Veuillez rentrer deux fois le mot de passe identique.',
+  'log_lenght_psw' => 'Votre mot de passe doit comporter entre 8 et 64 caractères.',
+  'log_captcha' => 'Le code de vérification antispam est incorrect',
+  'log_use_username' => 'Ce nom d\'utilisateur est déjà utilisé&#8239;!',
+  'log_use_mail' => 'Cette adresse e-mail est déjà utilisée&#8239;!',
+  'err_message' => 'Erreur, veuillez réessayer',
+  'form_username' => 'Nom d\'utilisateur&nbsp;:',
+  'form_mail' => 'Adresse e-mail&nbsp;:',
+  'form_psw1' => 'Mot de passe&nbsp;:',
+  'form_generate_psw' => 'Générer un mot de passe',
+  'form_psw2' => 'Mot de passe (vérification)&nbsp;:',
+  'form_subscribe_nl' => 'S\'inscrire à la lettre d\'information&nbsp;:',
+  'form_nl_freq_weekly' => '(mail hebdomadaire pour rester informer des mises à jours)',
+  'enable_js' => 'Activez JavaScript si vous souhaitez générer un mot de passe via le site',
+  'form_use_cookies' => <<<ENDSTR
+    <p>L'usage des cookies est nécessaire pour utiliser l'espace membres. Vous créer un compte <?= $site_name ?> confirme que vous acceptez les cookies en vous identifiant.<br>Nous ne partagerons pas votre adresse e-mail avec des tiers. Vous pourrez modifier les paramètres de votre compte ou le supprimer à tout moment.</p>
+  ENDSTR,
+  'form_submit' => 'S\'inscrire',
+];
