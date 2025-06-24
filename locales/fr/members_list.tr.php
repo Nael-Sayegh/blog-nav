00a34b9b6237d3d89e9467bf64b51b07c43a8dc5
Nael Sayegh <61361779+Nael-Sayegh@users.noreply.github.com>
2025-06-24 16:56:36 +0200

    Apply modif, translations, remove file (#2)

---
+++ locales/fr/members_list.tr.php
@@ -0,0 +1,18 @@
+<?php
+
+$tr = [
+  '_' => 'members_list',
+  '_todo_level' => 0,
+  '_last_author' => 'Nael Sayegh',
+  '_last_modif' => 1750776404,
+  'title' => 'Liste des membres {{site}}',
+  'sort_by' => 'Trier par&nbsp;:',
+  'sort_by_signup_date' => 'Date d\'inscription',
+  'sort_by_alpha' => 'Ordre alphabétique',
+  'enable_js' => '<p>Activez JavaScript pour trier les membres</p>',
+  'table_id_member' => 'Numéro de membre',
+  'table_name' => 'Nom',
+  'table_signup' => 'Inscription',
+  'table_rank' => 'Rang',
+  'table_count_members' => '<p><b>{{count}}</b> membres trouvés</p>',
+];
