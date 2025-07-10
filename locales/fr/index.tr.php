678e5c42f35a00c8b11d9cb5bc41839d58f4ec35
Nael-Sayegh <infos@nael-accessvision.com>
2025-07-10 11:10:45 +0200

    Fix text of presentation

--- locales/fr/index.tr.php
+++ locales/fr/index.tr.php
@@ -4,7 +4,7 @@ $tr = [
   '_' => 'index',
   '_todo_level' => 0,
   '_last_author' => 'Pascal',
-  '_last_modif' => 1751914169,
+  '_last_modif' => 1752138645,
   'title' => '{{site}}&nbsp;: {{slogan}}',
   'happynewyear' => '<p role="alert">Toute l\'équipe {{site}} vous souhaite une excellente année {{year}}, vous envoie ses voeux de bonheur, de santé et de sérénité.<br>
 
@@ -15,7 +15,7 @@ Nous espérons vous apporter satisfaction et répondre à tous vos besoins futur
     <p>Bienvenue sur {{site}}, votre espace dédié à l'accessibilité numérique et à l'informatique pour tous.<br>
     Vous trouverez ici des tutoriels audio sur des applications pour téléphone et ordinateur, des démonstrations d'appareils adaptés, des logiciels à télécharger, des astuces pratiques et des retours de tests pour vous accompagner au quotidien.<br>
     Le blog propose aussi des ressources sur les loisirs et lieux accessibles, des add-ons pour NVDA, et bien plus encore, afin de faciliter l'autonomie et l'inclusion numérique.<br>
-    N'hésitez pas à explorer les différentes rubriques, à vous abonner à la <a href="newsletter.php">newsletter</a> pour rester informé.<br><br>
+    N'hésitez pas à explorer les différentes rubriques, à vous abonner à la <a href="newsletter.php">newsletter</a> pour rester informé.<br>
     Si vous avez besoin de formation, d'assistance ou d'aide personnalisée en informatiqueou démarches administratives en ligne, rendez-vous sur notre site dédié&nbsp;:<br>
     <a href="https://nael-accessvision.com">nael-accessvision.com</a> pour découvrir nos services professionnels.</p><br aria-hidden="true">
   ENDSTR,
