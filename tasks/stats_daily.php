ae96b607107aa90a71f094d5c35e6719e06d6dc8
Nael-Sayegh <infos@nael-accessvision.com>
2025-07-20 17:24:09 +0200

    Restore stat_daily file

--- tasks/stats_daily.php
+++ tasks/stats_daily.php
@@ -18,7 +18,7 @@ $SQL = <<<SQL
     SQL;
 $req = $bdd->prepare($SQL);
 $req->execute([':beg' => time() - 86400, ':end' => time()]);
-foreach ($bdd->query($SQL) as $data)
+while ($data = $req->fetch())
 {
     if (isset($visitors[$data['domain']]))
     {
