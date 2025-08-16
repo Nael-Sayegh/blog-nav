b0ff34991e274938810fd4b6b53435eb6cd98440
Nael Sayegh <61361779+Nael-Sayegh@users.noreply.github.com>
2025-08-16 14:57:39 +0200

    apply modif from nav site (#16)
    Co-authored-by: Nael-Sayegh <infos@nael-accessvision.com>

--- locales/fr/index.tr.php
+++ locales/fr/index.tr.php
@@ -4,7 +4,7 @@ $tr = [
   '_' => 'index',
   '_todo_level' => 0,
   '_last_author' => 'Pascal',
-  '_last_modif' => 1754294256,
+  '_last_modif' => 1755342349,
   'title' => '{{site}}&nbsp;: {{slogan}}',
   'happynewyear' => '<p role="alert">Toute l\'équipe {{site}} vous souhaite une excellente année {{year}}, vous envoie ses voeux de bonheur, de santé et de sérénité.<br>
 
