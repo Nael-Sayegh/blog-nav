8d97a1c39e148e6f18d3189840bf9a46a0b3337b
Nael-Sayegh <infos@nael-accessvision.com>
2025-10-06 11:47:49 +0200

    Remove additional db connection

--- include/dbconnect.php
+++ include/dbconnect.php
@@ -12,15 +12,3 @@ catch (PDOException $e)
     print 'Erreur de connexion à la base de données';
     error_log('DB connect error: '.$e->getMessage());
 }
-
-// Connexion à la base de données nav pour les tables d'authentification
-try
-{
-    $nav_bdd = new PDO('pgsql:host=pgsql;dbname=nav;', DB_USER, DB_PSW);
-    $nav_bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e)
-{
-    print 'Erreur de connexion à la base de données nav';
-    error_log('Nav DB connect error: '.$e->getMessage());
-}
