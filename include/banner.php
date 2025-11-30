4d7dae8a8c6f02635640387e186a65bd52357c6f
Nael-Sayegh <infos@nael-accessvision.com>
2025-11-30 19:37:38 +0100

    Update banner.php

--- include/banner.php
+++ include/banner.php
@@ -3,9 +3,9 @@
 <?php
 if (isset($_SERVER['HTTP_USER_AGENT']) && str_contains((string) $_SERVER['HTTP_USER_AGENT'], 'Trident'))
 {
-    require_once __DIR__ . '/include/trident.php';
+    require_once __DIR__ . '/trident.php';
 }
 require_once __DIR__ . '/loginbox.php';
-include __DIR__ . '/include/searchtool.php'; ?>
+require_once __DIR__ . '/include/searchtool.php'; ?>
 </header>
-<?php include __DIR__ . '/include/menu.php'; ?>
+<?php require_once __DIR__ . '/menu.php'; ?>
