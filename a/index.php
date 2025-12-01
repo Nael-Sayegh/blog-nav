0b0fd3b1f4f581533f7cac24deb7578769e2a2a3
Nael-Sayegh <infos@nael-accessvision.com>
2025-12-01 10:32:48 +0100

    Add label manage in a/index.php

--- a/index.php
+++ a/index.php
@@ -3,10 +3,14 @@
 declare(strict_types=1);
 
 $params = '';
-if (isset($_GET['id']) && preg_match('/\d+/', (string) $_GET['id']))
+if (isset($_GET['label']) && preg_match('/^[a-z0-9_]+$/i', (string) $_GET['label']))
 {
-    $params .= 'id='.$_GET['id'];
+    $params .= 'label='.$_GET['label'];
 }
+elseif (isset($_GET['id']) && preg_match('/^\d+$/', (string) $_GET['id']))
+ {
+     $params .= 'id='.$_GET['id'];
+ }
 
 if ($params !== '' && $params !== '0')
 {
